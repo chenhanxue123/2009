4060f8ede8f9e38a604c3eb72053ae3993fed7ea
chx <you@example.com>
2021-05-08 11:01:35 +0800

    proxy 拦截操作

---
+++ 5-07/m1.js
@@ -0,0 +1,15 @@
+let name = "mechen"
+let age = 22
+
+
+let obj = {
+    name,
+    age,
+    sex:"女"
+}
+
+let hello = function(){
+    console.log(hello);
+}
+
+export{obj,hello}
---
+++ 5-07/m2.js
@@ -0,0 +1,6 @@
+import {obj,hello} from "./m1.js"
+console.log(obj);
+
+
+
+hello()
